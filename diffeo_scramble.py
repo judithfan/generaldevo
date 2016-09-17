d5d700d356ba2a9a0d4a031bde54032d3fbbb162
Judy Fan <judithfan@gmail.com>
2016-09-16 20:01:12 -0400

    full loop

--- diffeo_scramble.py
+++ diffeo_scramble.py
@@ -37,43 +37,45 @@ for f in furniture:
 # put name list together
 all_names = car_names + furniture_names
 
-# load in image
-URL = 'https://s3.amazonaws.com/morphrecog-images-1/' + all_names[0]
-file = cStringIO.StringIO(urllib.urlopen(URL).read())
-im = Image.open(file)
+for fn in all_names:
 
-# give user feedback about progress
-print 'loading ', all_names[0], ' at ', strftime("%Y-%m-%d %H:%M:%S")
+	# load in image
+	URL = 'https://s3.amazonaws.com/morphrecog-images-1/' + fn
+	file = cStringIO.StringIO(urllib.urlopen(URL).read())
+	im = Image.open(file)
 
-# im = Image.open("original/car.png")
-# convert to array
-image = np.asarray(im)
+	# give user feedback about progress
+	print 'loading ', fn, ' at ', strftime("%Y-%m-%d %H:%M:%S")
 
-rows, cols = image.shape[0], image.shape[1]
-src_cols = np.linspace(0, cols, 20)
-src_rows = np.linspace(0, rows, 20)
-src_rows, src_cols = np.meshgrid(src_rows, src_cols)
-src = np.dstack([src_cols.flat, src_rows.flat])[0]
+	# im = Image.open("original/car.png")
+	# convert to array
+	image = np.asarray(im)
 
-# add cosine oscillation to row coordinates
-dst_rows = src[:, 1] - np.random.randint(30,70,len(src[:, 1])) * \
-           np.cos(np.linspace(0, 3 * np.pi, src.shape[0]) + np.random.randint(1,10,len(src[:, 1])))
-dst_cols = src[:, 0] 
-dst_rows *= 1.5
-dst_rows -= 1.5 * 50
-dst = np.vstack([dst_cols, dst_rows]).T
+	rows, cols = image.shape[0], image.shape[1]
+	src_cols = np.linspace(0, cols, 20)
+	src_rows = np.linspace(0, rows, 20)
+	src_rows, src_cols = np.meshgrid(src_rows, src_cols)
+	src = np.dstack([src_cols.flat, src_rows.flat])[0]
 
-# derive transformation matrix
-tform = PiecewiseAffineTransform()
-tform.estimate(src, dst)
+	# add cosine oscillation to row coordinates
+	dst_rows = src[:, 1] - np.random.randint(30,70,len(src[:, 1])) * \
+	           np.cos(np.linspace(0, 3 * np.pi, src.shape[0]) + np.random.randint(1,10,len(src[:, 1])))
+	dst_cols = src[:, 0] 
+	dst_rows *= 1.5
+	dst_rows -= 1.5 * 50
+	dst = np.vstack([dst_cols, dst_rows]).T
 
-out_rows = image.shape[0] - 1.5 * 50
-out_cols = cols
-out = warp(image, tform, output_shape=(out_rows, out_cols))
+	# derive transformation matrix
+	tform = PiecewiseAffineTransform()
+	tform.estimate(src, dst)
 
-# turn black pixels gray
-out[(out==0)] = 0.5
+	out_rows = image.shape[0] - 1.5 * 50
+	out_cols = cols
+	out = warp(image, tform, output_shape=(out_rows, out_cols))
 
-# save out diffeo scrambled image
-result = Image.fromarray((out * 255).astype(np.uint8))
-result.save('diffeo/'+all_names[0].split('.')[0]+'.png')
+	# turn black pixels gray
+	out[(out==0)] = 0.5
+
+	# save out diffeo scrambled image
+	result = Image.fromarray((out * 255).astype(np.uint8))
+	result.save('diffeo/'+fn.split('.')[0]+'.png')
